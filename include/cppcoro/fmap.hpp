773a4be85849ded63cbf57d9016172395bc779b1
Lewis Baker <lewissbaker@gmail.com>
2017-07-17 07:38:38 +0930

    Add missing #includes to fmap.hpp
    Also delcare fmap_transform constructor explict and add noexcept
    declaration.

--- include/cppcoro/fmap.hpp
+++ include/cppcoro/fmap.hpp
@@ -5,12 +5,18 @@
 #ifndef CPPCORO_FMAP_HPP_INCLUDED
 #define CPPCORO_FMAP_HPP_INCLUDED
 
+#include <utility>
+#include <type_traits>
+
 namespace cppcoro
 {
 	template<typename FUNC>
 	struct fmap_transform
 	{
-		fmap_transform(FUNC&& f) : func(std::forward<FUNC>(f)) {}
+		explicit fmap_transform(FUNC&& f)
+			noexcept(std::is_nothrow_move_constructible_v<FUNC>)
+			: func(std::forward<FUNC>(f))
+		{}
 
 		FUNC func;
 	};
