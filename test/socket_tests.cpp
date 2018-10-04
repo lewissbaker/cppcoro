3d90fff93f884ffb114e6584fa9ca12dd02a81aa
Lewis Baker <lewissbaker@gmail.com>
2018-10-03 22:00:30 -0700

    Re-enable disabled test under msvc 2017.9 Preview 3 or later.
    The bug seems to have been fixed now.

--- test/socket_tests.cpp
+++ test/socket_tests.cpp
@@ -206,9 +206,10 @@ TEST_CASE("send/recv TCP/IPv4")
 		}()));
 }
 
-#if !CPPCORO_COMPILER_MSVC || !CPPCORO_CPU_X86
+#if !CPPCORO_COMPILER_MSVC || CPPCORO_COMPILER_MSVC >= 191626926 || !CPPCORO_CPU_X86
 // HACK: Don't compile this function under MSVC x86.
 // It results in an ICE under VS 2017.8 / 2017.9 Preview 1.
+// Seems to be fixed in 2017.9 Preview 3
 
 TEST_CASE("send/recv TCP/IPv4 many connections")
 {
